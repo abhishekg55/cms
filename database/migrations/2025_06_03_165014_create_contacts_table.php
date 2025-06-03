773f4c93509ffd131a321394a805ebf8dfdfb688
abhishekg71 <abhishek.gupta@camplus.co.in>
2025-06-03 22:52:24 +0530

    .

---
+++ database/migrations/2025_06_03_165014_create_contacts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->char('phone', 12);
+            $table->boolean('gender')->default(0)->comment('0 => male, 1 => Female');
+            $table->string('profile_image');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
